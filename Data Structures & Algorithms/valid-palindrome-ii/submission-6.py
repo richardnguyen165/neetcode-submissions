b394ed4e0e651ef64fe72d1ecd21e2bdcb655807
Richard Nguyen <151104924+richardnguyen165@users.noreply.github.com>
2026-07-03 14:59:35 -0600

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        # Error_counter => the amount of letters you can replace (capped at 1)
+
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -=1
+            else:
+                # Use slicing at the first occurance
+
+                # Skip over the left, include the right
+                left_option = s[left + 1: right + 1] == s[left + 1: right + 1][::-1]
+                
+                # Skip over the right, include the left
+                right_option = s[left : right] == s[left : right][::-1]
+
+                # Simple check if we skip either one, check if the other option is available
+                # We are only allowed one mistake, so this is the final decision
+
+                '''
+                Error: Only trying one deletion option, you must try both
+                '''
+                if left_option:
+                    return True
+                elif right_option: 
+                    return True
+                else:
+                    return False
+        return True
