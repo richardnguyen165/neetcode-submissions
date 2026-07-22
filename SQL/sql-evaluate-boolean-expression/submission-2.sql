710a00b224166598506eb350eff2f22c2b02d085
Richard Nguyen <151104924+richardnguyen165@users.noreply.github.com>
2026-07-22 09:09:31 -0600

    Add: sql-evaluate-boolean-expression - submission-2

---
+++ SQL/sql-evaluate-boolean-expression/submission-2.sql
@@ -0,0 +1,19 @@
+-- Write your query below
+SELECT left_operand, 
+operator, 
+right_operand, 
+CASE
+    WHEN operator = '>' THEN left_value > R.value
+    WHEN operator = '<' THEN left_value < R.value
+    ELSE left_value = R.value
+END AS value
+FROM (
+    SELECT value as left_value, left_operand, operator, right_operand
+    FROM variables L
+    INNER JOIN expressions E
+    ON E.left_operand = L.name) 
+AS L2
+INNER JOIN variables R
+ON L2.right_operand = R.name;
+-- An INNER JOIN returns only rows that have matching values in both tables
+-- (So, it does not return any null values)
