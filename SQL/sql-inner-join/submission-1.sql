2b9c588c79685863e1d8e4a1883e633ad8d5b9e4
Richard Nguyen <151104924+richardnguyen165@users.noreply.github.com>
2026-07-24 17:44:33 -0600

    Add: sql-inner-join - submission-1

---
+++ SQL/sql-inner-join/submission-1.sql
@@ -0,0 +1,40 @@
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE course_registrations (
+    id INT PRIMARY KEY,
+    student_id INT,
+    course_name TEXT
+);
+
+INSERT INTO students (id, name) VALUES
+(1, 'Adam Smith'),
+(2, 'Thomas Jefferson'),
+(3, 'John Adams'),
+(4, 'James Madison');
+
+INSERT INTO course_registrations (id, student_id, course_name) VALUES
+(1, 1, 'Math'),
+(2, 1, 'Science'),
+(3, 2, 'Math'),
+(4, 2, 'History'),
+(5, 3, 'Science'),
+(6, 3, 'Math'),
+(7, 4, 'History'),
+(8, 4, 'Math');
+-- Do not modify above this line. --
+
+-- INNER JOIN: Combine rows from tables based on matching values in a specified column.
+-- Only return rows when there is a match of values in a specified column
+
+SELECT s.name, cr.course_name
+FROM students s
+INNER JOIN course_registrations cr
+ON cr.student_id = s.id
+ORDER BY s.name;
+
+
+
+
