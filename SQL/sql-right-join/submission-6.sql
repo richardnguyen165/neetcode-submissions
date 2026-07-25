e42cf2815e6934223f7b375bc55af4b5520b78ac
Richard Nguyen <151104924+richardnguyen165@users.noreply.github.com>
2026-07-24 18:11:36 -0600

    Add: sql-right-join - submission-6

---
+++ SQL/sql-right-join/submission-6.sql
@@ -0,0 +1,34 @@
+CREATE TABLE pokemon_types (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    weakness TEXT
+);
+
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type_id INTEGER REFERENCES pokemon_types(id)
+);
+
+INSERT INTO pokemon_types (id, name, weakness) VALUES
+(1, 'Fire', 'Water'),
+(2, 'Water', 'Grass'),
+(3, 'Grass', 'Fire'),
+(4, 'Electric', 'Ground');
+
+INSERT INTO pokemon (id, name, type_id) VALUES
+(4, 'Charmander', 1),
+(7, 'Squirtle', 2),
+(3, 'Bulbasaur', 3);
+-- Do not modify above this line. --
+
+-- RIGHT JOIN: Same as left join, but all right rows are include, and if there is no match, left values are nulled
+
+SELECT pt.name AS type, p.name AS pokemon, pt.weakness 
+FROM pokemon p
+RIGHT JOIN pokemon_types pt
+ON p.type_id = pt.id
+ORDER BY pt.name, p.name, pt.weakness;
+
+
+
